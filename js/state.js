76e45fbb98e3b344812395ac78df185f8b5c25de
Danil Vorontsov <rojerr@MacBook-Air-Danil.local>
2023-09-13 12:41:11 +0600

    init

---
+++ js/state.js
@@ -0,0 +1,19 @@
+export default {
+  url: `https://v6.exchangerate-api.com/v6/aa3e520289f21c096ac17e19`,
+  codes: [],
+  pair: {
+    from: '',
+    to: '',
+  },
+  amount: '',
+  loading: false,
+  currentTab: 'convert',
+  currency: {
+    code: 'USD',
+  },
+  currencies: ['USD', 'EUR', 'BYN'],
+  actions: {
+    remove: 'remove',
+    change: 'change',
+  },
+};
